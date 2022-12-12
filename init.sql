7e2ec27f0c97c112ab0ada00f0d6c65cdf39972f
aic513 <aic513@mail.ru>
2022-12-13 02:35:46 +0300

    update docker-compose configs

--- init.sql
+++ init.sql
@@ -1,25 +1,28 @@
 DROP TABLE IF EXISTS request;
-DROP TABLE IF EXISTS response;
+DROP TABLE IF EXISTS response_headers;
 
 CREATE TABLE if not exists request
 (
     id INT AUTO_INCREMENT,
     request_url TEXT NOT NULL,
+    body TEXT,
+    status_code INT,
+    curl_status TEXT,
     created_at timestamp default current_timestamp,
+    updated_at timestamp,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE if not exists response
+CREATE TABLE if not exists response_headers
 (
     id INT AUTO_INCREMENT,
     request_id INT NOT NULL,
-    status_code TINYINT,
-    body TEXT,
-    header_key VARCHAR(50),
-    header_value VARCHAR(100),
+    header_key VARCHAR(100),
+    header_value VARCHAR(500),
     created_at timestamp default current_timestamp,
+    updated_at timestamp,
     PRIMARY KEY(id),
     CONSTRAINT fk_request_id FOREIGN KEY (request_id) REFERENCES request (id) ON DELETE CASCADE
 );
 
-CREATE INDEX key_value ON response (header_key, header_value)
+CREATE INDEX key_value ON response_headers (header_key, header_value)
